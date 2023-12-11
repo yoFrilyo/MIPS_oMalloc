1e10866baa128da920c9891647c33c477ece6ed2
Frilyo <142653518+yoFrilyo@users.noreply.github.com>
2023-12-10 20:37:20 -0800

    Add files via upload

--- Main.asm
+++ Main.asm
@@ -1,11 +1,10 @@
 # Main.asm
 
 ##################################################
+# Test driver for Malloc.asm
 #
-#
-#
-#
-#
+# The main function demonstrates how to use the
+# functions in Malloc.asm. 
 ##################################################
 
 .data
@@ -14,18 +13,65 @@
 
 .text
 main:
+    li $t0, 200			# 256 will give 
     
-    li $v0, 9
-    li $a0, 4
-    syscall
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s1, 4($sp)		# should be 0x10040008
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
     
-    sw $a0, 10040000
-    sw $a0, 1($v0)
-    sw $a0, 2($v0)
-    sw $a0, 3($v0)
-    sw $a0, 4($v0)
-    sw $a0, 5($v0)
-
-    li $v0, 0   # End program
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s2, 4($sp)		# should be 0x10040008
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
+    
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s3, 4($sp)		# should be 0x10040008
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
+    
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s4, 4($sp)		# should be 0x10040008
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
+    
+    addiu $sp, $sp, -8
+    sw $s3, 0($sp)
+    sw $t0, 4($sp)
+    jal free
+    lw $t0, 4($sp)
+    lw $s3, 0($sp)
+    addiu $sp, $sp, 8
+    
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s5, 4($sp)		# should be equal to $s3
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
+    
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s6, 4($sp)		# should be 0
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
+    
+    addiu $sp, $sp, -8
+    sw $t0, 0($sp)
+    jal malloc
+    lw $s7, 4($sp)		# should be 0
+    lw $t0, 0($sp)
+    addiu $sp, $sp, 8
+    
+    li $v0, 10   # End program
     syscall    
 # end main
--- Malloc.asm
+++ Malloc.asm
@@ -2,104 +2,113 @@
 # Owen Gallegos
 
 ##################################################
+# maxHeapSize:	amount of available space on the
+#   heap in bytes. Default is 1024 (1KB)
+#
+# blockCounter:	stores the number of blocks on the
+#   heap to know when to call sbrk. Do not change
+#   this value.
+#
+#
+#
 # .globl malloc:
 # in:   0($sp) = amount of bytes to store
-# out:  4($sp) = address of data block
+# out:  4($sp) = address of data block (0 if there
+#   is no more space available on the heap)
 # def:  Finds next available block of memory that
 #   can store the specified amount of bytes, then
 #   returns the address or 0 if there is not enough
 #   space on the heap.
+# uses:	$v: 0
+#	$a: 0
+#	$t: 0, 1, 2, 3
 #
-# .globl realloc:
-# in:   0($sp) = address of data block
-#       4($sp) = new amount of bytes to store
-# out:  8($sp) = new address of data block
-# def:  Finds next available block of memory that
-#   can store the specified amount of bytes. If the
-#   specified amount of bytes is smaller than the
-#   previous amount of allocated data, the address
-#   data may be lost.
+#
 #
 # .globl free:
 # in:   0($sp) = address of data block
 # out:  NULL
-# def:  Deallocates space
+# def:  Deallocates block specified on the heap.
+# uses: $t: 0, 1
 ##################################################
 .data
 
-    memSpaceTracker: .word 0
+    maxHeapSize: .word 1024
+    blockCounter: .word 0		# do not change this variable
 
 
         
 .text
-.globl malloc, realloc, free
-
-#malloc:
-    li $t1, 0($sp)          # load amount of bytes to store
-
-    addiu $sp, $sp, -8      # allocate stack space
-    sw $t1, 0($sp)          # save amount of bytes to store
-    sw $ra, 6($sp)
-    jal find                # call find
-    lw $ra, 6($sp)
-    lw $t0, 4($sp)          # load found address from return
-    lw $t1, 0($sp)          # load amount of bytes to store
-    addiu $sp, $sp, 8       # deallocate stack space
-
-    sw $t0, 4($sp)          # save found address to return
-    jr $ra                  # return
-# end malloc
-
-
-
-realloc:
-
-
-
-# end realloc
-
-
-
-free:
-    li $t0, 0($sp)          # load address of block to free
-    addi $t0, $t0, -4       # load address of in use variable
-    li $t1, 0               # 0 means block is freed
-    sw $t1, ($t0)           # set block in use to false
-# end free
+.globl malloc, free
 
 
 
 malloc:
-    li $t0, 0($sp)          # load amount of bytes to store
-    addiu $t0, $t0, 8       # add two words to $t0 (1st is next block address, 2nd is size of block)
-    li $t1, memSpaceTracker # load memSpaceTracker
-    li $t2, 0               # load heap address counter
-
+    lw $a0, 0($sp)          # load amount of bytes to store
+    
+    addi $a0, $a0, 8	    # add 2 words to bytes to store (1st is for next block address, 2nd is for block state)
+    li $t0, 0x10040000	    # load current heap address
+    lw $t1, blockCounter    # load blockCounter ($t1 = temp block counter)
+    
     findLoop:
-        add $t3, $t2, $t0       # $t3 = heap counter + amount of bytes to store
-        bgeu $t3, $t1, sbrk     # if required space is not available, increase heap space
-
-        lw $t4, 4($t2)          # load size of current block
-
-        lw $t2, ($t2)           # load next heap address at heap address counter
-        j findLoop
-        
-        sbrk:
-            sub $a0, $t1, $t2
-            sub $a0, $t0, $a0       # calculate amount of space to increase
-            addiu $a0, $a0, 4       # add 4 bytes
-            li $v0, 9               # 9 is sbrk
-            syscall                 # address stored in $v0
-            j returnMalloc
-        # end sbrk
+    	beqz $t1, sbrk		# call sbrk if there are no more blocks to check
+    	
+    	lw $t2, 4($t0)		# load state of block (in use = 1, free = 0)
+    	bnez $t2, incompatible	# branch to incompatible if block is in use
+    	
+    	lw $t3, ($t0)		# load next block address
+    	sub $t3, $t3, $t0	# calculate size of current block
+    	ble $a0, $t3, store	# branch to store if block is large enough, else block is incompatible
+    	
+    	incompatible:
+    	    lw $t0, ($t0)	# move to next address in heap
+    	    addi $t1, $t1, -1	# decrement temp block counter
+    	    j findLoop		# jump to findLoop
+    	# end incompatible
     # end findLoop
-    returnMalloc:
-        addiu $t0, $t0, -4      # get block in use word
-        li $t5, 1               # 1 means block is in use
-        sw $t5, ($t0)           # set block in use to true
-        addiu $t0, $t0, 4       # set $t0 to first index in block
-        
-        sw $t0, 4($sp)          # return address of first index in block
-        jr $ra                  # return
-    # end return
-# end find
+   
+    sbrk:
+    	lw $t1, maxHeapSize		# change $t1 to maxHeapSize
+    	addiu $t1, $t1, 0x10040000	# set $t1 to last index of heap
+    	addu $t2, $a0, $t0		# change $t2 to address of next block
+    	bgt $t2, $t1, noSpace		# branch to noSpace if the block does not have enough space to store
+    	li $v0, 9			# 9 is sbrk by $a0 bytes
+    	syscall				# heap space is made available
+    	sw $t2, ($t0)			# save address to address section of current block
+    # end sbrk
+    	
+    store:
+    	lw $t2, blockCounter	# change $t2 to block counter
+    	addiu $t2, $t2, 1	# increment $t2
+    	sw $t2, blockCounter	# increment blockCounter
+    	
+    	li $t1, 1	    	# change $t1 to 1
+    	sw $t1, 4($t0)	    	# change block state to in use
+    	addiu $t0, $t0, 8   	# change $t0 to 1st index of actual block
+    	
+    	sw $t0, 4($sp)	    	# return address of block
+    	jr $ra		   	# return
+    # end store
+    
+    noSpace:
+    	li $t0, 0	    # change $t0 to 0
+    	sw $t0, 4($sp)	    # return 0
+    	jr $ra		    # return
+    # end noSpace
+# end malloc
+
+
+
+free:
+    lw $t0, 0($sp)          # load address of block to free
+    
+    lw $t1, blockCounter    # load blockCounter
+    addi $t1, $t1, -1       # decrement $t1
+    sw $t1, blockCounter    # decrement blockCounter
+    
+    addi $t0, $t0, -4       # load address of in use variable
+    li $t1, 0               # 0 means block is freed
+    sw $t1, ($t0)           # set block in use to false
+    
+    jr $ra		    # return
+# end free
